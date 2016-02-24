b203da9bef7dde0d2474957615779f411ffa7f7d
Martin Ben <mebenav88@gmail.com>
2016-02-24 00:05:05 -0500

    add App.propTypes and defaultProps to App.js

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render(){
-    return <div>Hi</div>
+    // one way
+    let txt = this.props.txt
+    return <div>{txt}</div>
+
+    // another way to return
+    // return React.createElement('h1', null, "Hello Guys")
+
+    //another way
+    // return (
+    //   <div>
+    //     <h1>Hello World</h1>
+    //     <b>bold</b>
+    //   </div>
+    // )
   }
 }
 
-export default App
+//const App = () => <h1>Hello Eggheads</h1>
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default text'
+}
+
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('app')
+)
+
+
+// export default App
